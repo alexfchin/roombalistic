0edd2868810fd69be3f1240ca6e3698c8e579691
Yang Yang <yangyang1998@gmail.com>
2019-02-16 22:13:03 +0000

    Revert "Continued work on facial recognition"
    This reverts commit c84125161704449c1ac859080c55162dac878475.

--- recognition.py
+++ recognition.py
@@ -1,23 +1,13 @@
-from PIL import Image
 import face_recognition
-import os
 
-known_image_folder = "./Photos/Known/"
-unknown_image = "./Photos/2120.jpg"
+image = face_recognition.load_image_file("/home/yang28/Pictures/roombalistic/2120.jpg");
 
-directory = os.fsencode(known_image_folder)
-known_images = [face_recognition.load_image_file(file)
-        for file in os.listdir(directory)]
+face_locations = face_recognition.face_locations(image)
 
-try:
-    unknown_image_encoding = face_recognition.face_encodings(unknown_image)[0]
-    known_image_encodings = [face_recognition.face_encodings(image)[0]
-            for image in known_images]
-except IndexError:
-    print("Could not detect any faces in at least one of the known images.")
-    quit()
+print("Number of faces: {}".format(len(face_locations)))
 
-results = face_recognition.compare(known_image_encodings, unknown_image_encoding)
-
-for result in results
-    print(result)
+for location in face_locations:
+	print(location)
+	top, right, bottom, left = face_location
+	
+	# do stuff
