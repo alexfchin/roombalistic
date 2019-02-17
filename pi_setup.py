4d2efd0eba0977c77d5d3c55ad37758f3272b368
Yang Yang <yangyang1998@gmail.com>
2019-02-17 09:40:31 -0500

    11th hour

--- pi_setup.py
+++ pi_setup.py
@@ -1,5 +1,3 @@
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 import gcp_storage
 import laser
 import time
@@ -16,65 +14,34 @@ local_id = '/home/pi/Documents/hack@CEWIT19/roombalistic/id.txt'
 # casual global variable for cyclic event-handling
 isProcessing = False
 
-class Frame_Event(FileSystemEventHandler):
-	def __init__(self, observer, filename):
-		self.observer = observer
-		self.filename = filename
+def frame_upload():
+    isProcessing = True
+    print(event)
+    gcp_storage.upload_blob(bucket, local_frame, gcp_frame)
 
-	def on_modified(self, event):
-		frame_upload(event)
+    try:
+        time.sleep(1)
+        gcp_storage.download_blob(bucket, local_id, gcp_id)
+    except KeyboardInterrupt:
+        id_observer.stop()
 
-class ID_Event(FileSystemEventHandler):
-	def __init__(self, observer, filename):
-		self.observer = observer
-		self.filename = filename
+    id_observer.stop()
 
-	def on_modified(self, event):
-		id_download(event)
-
-def frame_upload(event):
-	isProcessing = True
-	print(event)
-	id_observer = Observer()
-	dir_path = '/home/pi/Documents/hack@CEWIT19/roombalistic/'
-	id_event_handler = ID_Event(id_observer, dir_path)
-	id_observer.schedule(id_event_handler, dir_path)
-	id_observer.start()
-	gcp_storage.upload_blob(bucket, local_frame, gcp_frame)
-
-	try:
-		while isProcessing:
-			gcp_storage.download_blob(bucket, local_id, gcp_id)
-			time.sleep(1)
-	except KeyboardInterrupt:
-		id_observer.stop()
-
-	id_observer.stop()
-
-def id_download(event):
-	with open(local_id, "r") as id_file:
-		identifiers = id_file.read()
-	print(identifiers)
-	if "Unknown" not in identifiers:
-		laser.ready_laser(True)
-	isProcessing = False
-	print(event)
+def id_download():
+    with open(local_id, "r") as id_file:
+        identifiers = id_file.read()
+    print(identifiers)
+    if "Unknown" not in identifiers:
+        laser.ready_laser(True)
+    isProcessing = False
+    print(event)
 
 if __name__ == "__main__":
 
-	while True:
-		# Upload the video frame to the GCP storage bucket
-		# and updates id file
-		dir_path = '/home/pi/Documents/hack@CEWIT19/imgs/'
-		observer = Observer()
-		frame_event_handler = Frame_Event(observer, dir_path)
-		observer.schedule(frame_event_handler, dir_path)
-		observer.start()
+    while True:
+        # Upload the video frame to the GCP storage bucket
+        # and updates id file
+        frame_upload()
 
-		try:
-			while not isProcessing:
-				time.sleep(1)
-		except KeyboardInterrupt:
-			observer.stop()
-
-		observer.join()
+        while isProcessing:
+            time.sleep(0.1)
