7a0bb286a01e526a4ea6ff47844528b6ea073826
Yang Yang <yangyang1998@gmail.com>
2019-02-17 01:51:25 +0000

    Implemented nearest k-neighbor method of facial recognition

--- recognition.py
+++ recognition.py
@@ -1,13 +1,22 @@
 import face_recognition
+import os
 
-image = face_recognition.load_image_file("/home/yang28/Pictures/roombalistic/2120.jpg");
+import face_recognition_knn
 
-face_locations = face_recognition.face_locations(image)
+known_images_dir = "./Photos/Known"
+unknown_image = "./Photos/2120.jpg"
+knn_model = "./trained_knn_model.clf"
 
-print("Number of faces: {}".format(len(face_locations)))
+def recognition(cropped_images):
+    face_recognition_knn.train(known_images_dir, model_save_path=knn_model, verbose=True)
 
-for location in face_locations:
-	print(location)
-	top, right, bottom, left = face_location
-	
-	# do stuff
+    identities = [face_recognition_knn.predict(image, model_path=knn_model)
+            for image in cropped_images]
+
+    return identities
+
+# For debugging purposes
+if __name__ == "__main__":
+    predictions = recognition([unknown_image])
+    face_recognition_knn.show_prediction_labels_on_image(unknown_image, predictions)
+    print(predictions)
