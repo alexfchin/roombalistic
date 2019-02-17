abc89725756e34920a7dc5e8d16b8ffa31ba0eba
Yang Yang <yangyang1998@gmail.com>
2019-02-17 06:43:09 -0500

    fixed processing output

--- gcp_setup.py
+++ gcp_setup.py
@@ -17,16 +17,23 @@ if __name__ == "__main__":
 	vision.extract_faces(vm_frame, 4)
 
 	# Identify the faces
-	images = [pic for pic in os.listdir("./temp/") if os.path.isfile(os.path.join("./temp/", pic)) and pic.endswith(".jpg")]
+	images = []
+	for pic in os.listdir("./temp/"):
+		pic_path = os.path.join("./temp/", pic)
+		if os.path.isfile(pic_path) and pic.endswith(".jpg"):
+			images.append(pic_path)
+
 	del images[0] # remove the temp image.jpg
 	print(images)
 	identities = recognition.recognition(images)
 	print(identities)
+	identities_string = "\n".join(map(str, identities))
+	print(identities_string)
 
 	# Write id info to file
 	vm_id = "./temp/id.txt"
 	id_file = open("./temp/id.txt", "w")
-	id_file.write("\n".join(identities))
+	id_file.write(identities_string)
 	id_file.close()
 
 	# Send identity information to GCP storage bucket
