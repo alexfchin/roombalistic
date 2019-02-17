a904bb0787f9510849a6d5590fc4dfd6efdbe181
Alex Chin <alex.chin23@gmail.com>
2019-02-17 02:32:43 -0500

    laser on recognition

--- laser.py
+++ laser.py
@@ -16,14 +16,23 @@ def setup():
 	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
 	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
 
-def loop():
-	while True:
-		print '...Laser on'
-		GPIO.output(LedPin, GPIO.LOW)  # led on
-		time.sleep(0.5)
-		print 'Laser off...'
-		GPIO.output(LedPin, GPIO.HIGH) # led off
-		time.sleep(0.5)
+def loop(identities):
+	#identities = [('unknown', (107, 239, 159, 187)), ('Rahul Sondhi', (87, 156, 149, 93))]
+
+
+	for person in identities:
+		if person[0] != 'unknown':
+			print('Laser on')
+			#GPIO.output(LedPin, GPIO.LOW)  # led on
+			time.sleep(1.0)
+	destroy()
+	# while True:
+	# 	print '...Laser on'
+	# 	GPIO.output(LedPin, GPIO.LOW)  # led on
+	# 	time.sleep(0.5)
+	# 	print 'Laser off...'
+	# 	GPIO.output(LedPin, GPIO.HIGH) # led off
+	# 	time.sleep(0.5)
 
 def destroy():
 	GPIO.output(LedPin, GPIO.HIGH)     # led off
