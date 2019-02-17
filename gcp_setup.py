391abfe3287115a931aaac67a0b22ccda151a627
Yang Yang <yangyang1998@gmail.com>
2019-02-17 05:11:17 -0500

    Removed download bug with GCP storage

--- gcp_setup.py
+++ gcp_setup.py
@@ -14,16 +14,21 @@ if __name__ == "__main__":
 	gcp_storage.download_blob(bucket, storage_frame, vm_frame)
 
 	# Detect and crop faces
-	vision.extract_faces(filename, 4)
+	vision.extract_faces(vm_frame, 4)
 
 	# Identify the faces
-	images = [pic for pic in os.listdir("./temp/") if os.path.isfile(os.path.join("./temp/", pic))]
+	images = [pic for pic in os.listdir("./temp/") if os.path.isfile(os.path.join("./temp/", pic)) and pic.endswith(".jpg")]
 	del images[0] # remove the temp image.jpg
 	print(images)
 	identities = recognition.recognition(images)
 	print(identities)
 
-	# Send identity information to GCP storage bucket
+	# Write id info to file
 	vm_id = "./temp/id.txt"
+	id_file = open("./temp/id.txt", "w")
+	id_file.write("\n".join(identities))
+	id_file.close()
+
+	# Send identity information to GCP storage bucket
 	storage_id = "id.txt"
 	gcp_storage.upload_blob(bucket, vm_id, storage_id)
