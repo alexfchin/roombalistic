fc36f855d0ec95e5b0e566e8f62c190b789bc18c
Yang Yang <yangyang1998@gmail.com>
2019-02-17 09:11:56 -0500

    tab/space issues

--- laser.py
+++ laser.py
@@ -9,26 +9,26 @@ import time
 LaserGPIO = 17 # --> PIN11/GPIO17
 
 def setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(LaserGPIO, GPIO.OUT)
-    GPIO.output(LaserGPIO, GPIO.HIGH)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(LaserGPIO, GPIO.OUT)
+	GPIO.output(LaserGPIO, GPIO.HIGH)
 
 def loop(fire):
-    if fire:
+	if fire:
 		GPIO.output(LaserGPIO, GPIO.HIGH) # led on
 		time.sleep(2.5)
-    destroy()
+	destroy()
 
 def destroy():
-    GPIO.output(LaserGPIO, GPIO.LOW)
-    GPIO.cleanup()
+	GPIO.output(LaserGPIO, GPIO.LOW)
+	GPIO.cleanup()
 
 def ready_laser(fire):
-    setup()
+	setup()
 	loop(fire)
 
 #try:
-#    loop(identities)
+#	loop(identities)
 
 #except KeyboardInterrupt:
-#    destroy()
+#	destroy()
